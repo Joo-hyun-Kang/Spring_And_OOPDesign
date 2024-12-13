593df201cc950481a3f238b309f34f5fad3540c6
Joo-hyun-Kang <autoba9687@gmail.com>
2024-12-14 03:24:14 +0900

    feat: Aircraft, Towerの初実装

---
+++ avaj/src/Tower.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tower {
+    private List<Flyable> observers = new ArrayList<>();
+
+    public void register(Flyable p_flyable) {
+        observers.add(p_flyable);
+    }
+
+    public void unregister(Flyable p_flyable) {
+        observers.remove(p_flyable);
+    }
+
+    protected void conditionChanged() {
+        //Aircarft関連クラスの実装完成後、実装
+        assert false;
+    }
+}
