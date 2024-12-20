f9bef0e48239459fbf20c6c8c801141a9aeefbd0
Joo-hyun-Kang <autoba9687@gmail.com>
2024-12-20 23:58:51 +0900

    chore: 他のプロジェクト追加するためのファイールの片付け

--- avaj/src/AircraftFactory.java
+++ avaj/src/AircraftFactory.java
@@ -2,16 +2,16 @@ import flyable.aircraft.Coordinates;
 import flyable.Flyable;
 
 public class AircraftFactory {
-    //staticフィールドはクラスロード時に1度だけメモリに読み込まれるため、グローバルな1つのインスタンスを保証します。
-    //いつメモリに載せる？　メモリはデータ領域に載せるか？
+    //staticクラスの関数はCの関数ようにメモリにはそれが呼ばれた時に載せる
+    //  -staticクラスの存在理由：インスタンスを作りたくない場合、Cように関数を作りたい
+    //static変数はどちらにあればいい？結局全域（全域変数）
     private static AircraftFactory aircraftFactory;
 
     //singleton class
     private AircraftFactory() {};
 
     //マルチタスクに対応なし
-
-    public AircraftFactory getInstance() {
+    public static AircraftFactory getInstance() {
         if (aircraftFactory == null) {
             aircraftFactory =  new AircraftFactory();
         }
